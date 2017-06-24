96a7b87c7e980fc2525c79b87ab453db977e0622
Ray nieva <Ray nieva>
2017-06-23 22:23:35 -0400

    new file:   Python/BudgetAndBilling.py 	new file:   Ruby/BudgetAndBilling.rb

---
+++ Python/BudgetAndBilling.py
@@ -0,0 +1,9 @@
+﻿# This program is intended to be used to calculate my weekly budget based on income that week and projected expenses that week.
+print("Calculating Weekly/ Budget")
+
+# This is usually weekly (net) income but can be another period like bi-weekly or monthly
+print("Get Income for week/period")
+income1 = float(input())
+
+# At this point start inputting billing items. These are essentially billing items for the current week or period. 
+# The question is at this point what direction this program will go? Will there be permanently assigned billing items or billing items adhoc defined each week. Can Flowgorithm define a true OO approach?
---
+++ Ruby/BudgetAndBilling.rb
@@ -0,0 +1,11 @@
+﻿include Math
+
+# This program is intended to be used to calculate my weekly budget based on income that week and projected expenses that week.
+puts "Calculating Weekly/ Budget"
+
+# This is usually weekly (net) income but can be another period like bi-weekly or monthly
+puts "Get Income for week/period"
+income1 = gets.chomp.to_f
+
+# At this point start inputting billing items. These are essentially billing items for the current week or period. 
+# The question is at this point what direction this program will go? Will there be permanently assigned billing items or billing items adhoc defined each week. Can Flowgorithm define a true OO approach?
