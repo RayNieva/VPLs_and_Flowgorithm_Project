f8dff57ce6df1ef884de10131132d2be3d52031a
Ray nieva <Ray nieva>
2017-06-23 21:51:09 -0400

    new file:   Java/BudgetAndBilling.java

---
+++ Java/BudgetAndBilling.java
@@ -0,0 +1,21 @@
+﻿import java.util.*;
+import java.lang.Math;
+import java.util.Scanner;
+
+class JavaApplication {
+    private static Scanner input = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        // This program is intended to be used to calculate my weekly budget based on income that week and projected expenses that week.
+        System.out.println("Calculating Weekly/ Budget");
+        
+        // This is usually weekly (net) income but can be another period like bi-weekly or monthly
+        System.out.println("Get Income for week/period");
+        double income1;
+        
+        income1 = input.nextDouble();
+        
+        // At this point start inputting billing items. These are essentially billing items for the current week or period. 
+        // The question is at this point what direction this program will go? Will there be permanently assigned billing items or billing items adhoc defined each week. Can Flowgorithm define a true OO approach?
+    }
+}
